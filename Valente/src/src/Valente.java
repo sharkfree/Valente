f4411d32c660326b787c05d31411a360cb55f115
shark <vwildt@hotmail.de>
2025-11-15 12:50:30 +0100

    útil

--- Valente/src/src/Valente.java
+++ Valente/src/src/Valente.java
@@ -4,7 +4,7 @@ public class Valente {
 
 	public static void main(String[] args) {
 		
-		System.out.println("buceta fudida profissional amante foda pau buceta");
+		System.out.println("buceta fudida profissional amante foda pau buceta útil");
 
 	}
 
