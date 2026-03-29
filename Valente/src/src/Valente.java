28cf49a8bc196445f446a4def5062a24335451b7
shark <vwildt@hotmail.de>
2026-03-29 12:37:43 +0200

    caralho

--- Valente/src/src/Valente.java
+++ Valente/src/src/Valente.java
@@ -6,8 +6,8 @@ public class Valente {
 		
 
 
-		System.out.println("buceta procura assistencia de pau dourado 
-		de ouro gostoso bonzao tarado  do ");
+		System.out.println("buceta procura assistencia de pau dourado "+
+		"de ouro gostoso bonzao tarado  do caralho");
 
 
 
