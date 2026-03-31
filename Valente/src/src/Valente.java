a40bcbda05ed80345e18e5448f5a14ad8ff1f135
shark <vwildt@hotmail.de>
2026-03-31 11:19:50 +0200

    dourado

--- Valente/src/src/Valente.java
+++ Valente/src/src/Valente.java
@@ -7,7 +7,7 @@ public class Valente {
 
 
 		System.out.println("buceta procura assistencia de pau dourado "+
-		"de ouro gostoso bonzao tarado  do caralho");
+		"de ouro gostoso bonzao tarado  do caralho dourado");
 
 
 
